eaecd9c5cd2a52ca6948b5b5f177561f7d2a9557
yodfz <yodfz@qq.com>
2018-02-27 14:25:44 +0800

    init Project

---
+++ wx-charts/index.js
@@ -0,0 +1,11 @@
+module.exports = function ($canvasId, $w, $h) {
+    const ctx = wx.createCanvasContext($canvasId)
+    return {
+        clear() {
+
+        },
+        drawLine($point, $color) {
+
+        }
+    }
+}
